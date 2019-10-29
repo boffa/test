d119972681feaea0ab7b7034dcd74302dee6f640
wafa <lakwafa@gmail.com>
2019-10-30 00:23:14 +0100

    add android lib module core

--- challenge/core/src/main/java/com/mobilite/core/domain/Url.kt
+++ challenge/core/src/main/java/com/mobilite/core/domain/Url.kt
@@ -1,7 +1,13 @@
 package com.mobilite.core.domain
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 @Parcelize
 data class Url(
-    val id: Int,
-    val url: String
-):Parcelable
+    val raw: String,
+    val full: String,
+    val small: String,
+    val regular: String,
+    val thumb: String
+): Parcelable
